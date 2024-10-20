756eefad97498a50801860b7271de22abc8e10d1
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-10-19 21:33:19 -0400

    comment code to show how to change code for different rulesets

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 func life(cells []int) []int {
 	newCells := make([]int, len(cells))
-	// Rules, if adjacent to a 1 it becomes alive
-	// If next to two 1s it dies
+	/* Serpinski Triangle Rules:
+	if adjacent to a 1 it becomes alive
+	If in between two 1s it dies */
+
+	// USER: update the 1 to a 0 or 0 to 1 for each case: newCells[i] = 1 to newCells[i] = 0
 	newCells[0] = 0
 	newCells[len(cells)-1] = 0
 	for i := 1; i < len(cells)-1; i++ {
@@ -51,12 +54,15 @@ func printCells(cells []int) {
 }
 
 func main() {
+	// USER: update the length of the slice 'cells' below to another odd number
 	cells := make([]int, 151)
 	cells[len(cells)/2+1] = 1
 
 	printCells(cells)
 
 	newIteration := cells
+	// USER: update the number of lines to be printed out in this for loop.
+	// If you want to print 333 lines, change to: i < 300
 	for i := 0; i < 101; i++ {
 		newIteration = life(newIteration)
 		printCells(newIteration)
